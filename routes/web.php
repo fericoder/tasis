b7cc664848331c0732f9ef7632d2f90f97ad8364
Farzaneh <farzane.a.shirazi@gmail.com>
2020-02-19 18:09:50 +0330

    index & services & contact are added

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('services', function () {
+    return view('services');
+});
+Route::get('contact', function () {
+    return view('contact');
 });
